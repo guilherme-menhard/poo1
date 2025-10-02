ebb52b472762edd21d25d94eff73461fdcd917e7
guilherme-menhard <guilhermemenhard.aluno@unipampa.edu.br>
2025-10-02 20:59:03 -0300

    classeDocumento

--- src/estudantes/entidades/Documento.java
+++ src/estudantes/entidades/Documento.java
@@ -1,12 +1,64 @@
 package estudantes.entidades;
 
+import professor.entidades.CodigoCurso;
+
 /**
  * Classe que representa um documento genérico.
  * <br><br>
  * <strong>Seu trabalho começa aqui...</strong>
  * 
- * @author coloque os nomes dos autores aqui
+ * @author Guilherme Menhard
  */
+
 public abstract class Documento {
+    private String criador;
+    private CodigoCurso codigoCurso;
+    private int paginas;
+
+    public Documento(String criador, CodigoCurso codigoCurso, int paginas) {
+        this.criador = criador;
+        this.codigoCurso = codigoCurso;
+        this.paginas = paginas;
+    }
     
+    public String getCriador() {
+        return criador;
+    }
+
+    public CodigoCurso getCodigoCurso() {
+        return codigoCurso;
+    }
+
+    public int getPaginas() {
+        return paginas;
+    }
+
+    public void setCriador(String criador) {
+        this.criador = criador;
+    }
+
+    public void setCodigoCurso(CodigoCurso codigoCurso) {
+        this.codigoCurso = codigoCurso;
+    }
+
+    public void setPaginas(int paginas) {
+        this.paginas = paginas;
+    }
+
+    public boolean equals(Documento doc) {
+        if (this.criador.equals(doc.getCriador()) && this.codigoCurso == doc.getCodigoCurso() && this.paginas == doc.getPaginas()) {
+            return true;
+        }
+        return false;
+    }
+
+/* Início do Código gerado por IA */
+    public int hashCode() {
+        int hash = 7;
+        hash = 31 * hash + (this.criador != null ? this.criador.hashCode() : 0);
+        hash = 31 * hash + (this.codigoCurso != null ? this.codigoCurso.hashCode() : 0);
+        hash = 31 * hash + this.paginas;
+        return hash;
+    }
+/* Fim do Código gerado por IA */
 }
