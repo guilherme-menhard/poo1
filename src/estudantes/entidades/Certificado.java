e81d2516bf0bff3b8d3738c1a80d0e6999b77646
Mariana Ferrao Chuquel <marianachuquel.aluno@unipampa.edu.br>
2025-10-03 12:40:53 -0300

    equals() e hashcode()

--- src/estudantes/entidades/Certificado.java
+++ src/estudantes/entidades/Certificado.java
@@ -19,4 +19,24 @@ public class Certificado extends Registro {
     public void setDescricao(String descricao) {
         this.descricao = descricao;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Certificado certificado = (Certificado) obj;
+        return descricao.equals(certificado.descricao);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + (descricao != null ? descricao.hashCode() : 0);
+        return result;
+    }
 }
