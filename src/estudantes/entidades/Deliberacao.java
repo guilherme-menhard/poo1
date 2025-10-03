ef4750ea5e95a6d3556166e2c10911740540fc74
guilherme-menhard <guilhermemenhard.aluno@unipampa.edu.br>
2025-10-02 21:52:15 -0300

    Deliberacao

--- src/estudantes/entidades/Deliberacao.java
+++ src/estudantes/entidades/Deliberacao.java
@@ -7,6 +7,34 @@ import professor.entidades.CodigoCurso;
  */
 
 
-public class Deliberacao {
+public class Deliberacao extends DocumentoAdministrativo {
     
+    private String texto;
+
+    public Deliberacao(String texto, String criador, CodigoCurso codigoCurso, int paginas) {
+        super(criador, codigoCurso, paginas);
+    }
+    
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public boolean equalsDeliberacao(Deliberacao delib) {
+        if (this.getTexto().equals(delib.getTexto()) && this.equalsDocumentoAdministrativo(delib)) {
+            return true;
+        }
+        return false;
+    }
+
+    public int hashCodeDeliberacao() {
+        int result = 17; 
+        result = 31 * result + (getTexto() != null ? getTexto().hashCode() : 0);
+        result = 31 * result + hashCodeDocumentoAdministrativo();
+        return result;
+    }
+
 }
