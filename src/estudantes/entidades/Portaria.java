619a29ef5d3bedc4872c0de9687dc77be1b1bfe5
guilherme-menhard <guilhermemenhard.aluno@unipampa.edu.br>
2025-10-02 22:00:28 -0300

    Portaria

--- src/estudantes/entidades/Portaria.java
+++ src/estudantes/entidades/Portaria.java
@@ -6,7 +6,34 @@ import professor.entidades.CodigoCurso;
  * @author Guilherme Menhard
  */
 
-
-public class Portaria {
+public class Portaria extends Norma {
     
+    private int anoInicio;
+
+    public Portaria(int anoInicio, int numero, boolean valido, String texto, String criador, CodigoCurso codigoCurso, int paginas) {
+        super(numero, valido, texto, criador, codigoCurso, paginas);
+        this.anoInicio = anoInicio;
+    }
+
+    public int getAnoInicio() {
+        return anoInicio;
+    }
+
+    public void setAnoInicio(int anoInicio) {
+        this.anoInicio = anoInicio;
+    }
+
+    public boolean equalsPortaria(Portaria port) {
+        if (this.getAnoInicio() == port.getAnoInicio() && this.equalsNorma(port)) {
+            return true;
+        }
+        return false;
+    }
+
+    public int hashCodePortaria() {
+        int result = 17; 
+        result = 31 * result + getAnoInicio();
+        result = 31 * result + hashCodeNorma();
+        return result;
+    }
 }
