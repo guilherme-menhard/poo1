f9c5d5a79196b3aa1a12e4c6960486bd8593f3f0
Mariana Ferrao Chuquel <marianachuquel.aluno@unipampa.edu.br>
2025-10-03 13:13:28 -0300

    equals e hash

--- src/estudantes/entidades/Historico.java
+++ src/estudantes/entidades/Historico.java
@@ -1,6 +1,7 @@
 //Mariana
 package estudantes.entidades;
 
+import java.util.Arrays;
 import professor.entidades.CodigoCurso;
 
 public class Historico extends Registro {
@@ -31,4 +32,24 @@ public class Historico extends Registro {
         this.componentes = componentes;
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Historico historico = (Historico) obj;
+        return Double.compare(coeficiente, historico.coeficiente) == 0
+                && Arrays.equals(componentes, historico.componentes);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Double.hashCode(coeficiente);
+        result = 31 * result + Arrays.hashCode(componentes);
+        return result;
+    }
 }
